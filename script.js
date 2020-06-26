fe1e0a2668fd5971b7711a8955ec1d897d35990e
josephden16 <tegajoseph2002@gmail.com>
2020-06-26 12:48:05 +0100

    worked on main components

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let hamburger = document.querySelector('.nav__hamburger');
 let mobileNav = document.querySelector('.mob-nav');
-let mockup = document.querySelector('.intro__mockup');
+let mockup = document.querySelector('.intro__container__mockup');
 hamburger.addEventListener('click', () => {
   mobileNav.classList.toggle('open');
   mockup.classList.toggle('close');
